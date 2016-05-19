551821f0a8e968ea78cb3ddf49a273fb86031c3c
Lauren N. Roth <laurennicoleroth@gmail.com>
2016-05-19 11:00:39 -0400

    Initial Commit

---
+++ ScavengerHunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScavengerHunt
+//
+//  Created by Lauren Nicole Roth on 5/19/16.
+//  Copyright © 2016 Lauren Nicole Roth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
