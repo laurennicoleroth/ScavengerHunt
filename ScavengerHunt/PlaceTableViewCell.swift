a24284b37b33387a30503436c4b921d7433612c7
Lauren N. Roth <laurennicoleroth@gmail.com>
2016-05-19 12:07:30 -0400

    Add place tableview cell. Empty

---
+++ ScavengerHunt/PlaceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlaceTableViewCell.swift
+//  ScavengerHunt
+//
+//  Created by Lauren Nicole Roth on 5/19/16.
+//  Copyright © 2016 Lauren Nicole Roth. All rights reserved.
+//
+
+import UIKit
+
+class PlaceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
