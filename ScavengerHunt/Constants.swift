e2a447f55b0d5be109cc181806ac167890ec8b69
Lauren N. Roth <laurennicoleroth@gmail.com>
2016-05-19 11:33:08 -0400

    Embed welcome controller in navigation controller

---
+++ ScavengerHunt/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  ScavengerHunt
+//
+//  Created by Lauren Nicole Roth on 5/19/16.
+//  Copyright © 2016 Lauren Nicole Roth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Constants: NSObject {
+    
+    struct Keys {
+        
+        static let GoogleKey = "AIzaSyDYVwDPOygjo-WyrAVT3P1zRhHHy7rjELc"
+        
+    }
+    
+    struct Url {
+        static let GoogleApiPlaceSearchJson = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+        static let GoogleApiPlaceSearchPhoto = "https://maps.googleapis.com/maps/api/place/photo"
+        static let GoogleApiPlaceSearchDetailsJson = "https://maps.googleapis.com/maps/api/place/details/json"
+    }
+    
+}
--- ScavengerHunt/WelcomeViewController.swift
+++ ScavengerHunt/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  ScavengerHunt
 //
 //  Created by Lauren Nicole Roth on 5/19/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
