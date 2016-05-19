d68479de8ee077d0d15f22ae38e199075d9e55a3
Lauren N. Roth <laurennicoleroth@gmail.com>
2016-05-19 12:24:29 -0400

    Connect outlets from museum place cell to UITableViewCell class

--- ScavengerHunt/PlaceTableViewCell.swift
+++ ScavengerHunt/PlaceTableViewCell.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class PlaceTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var priceLevel: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var rating: UILabel!
+    @IBOutlet weak var starRating: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
      
-        
+        photo.layer.masksToBounds = true
+        photo.contentMode = .ScaleAspectFill
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
