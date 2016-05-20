d12ff36d8f1bb6e8115a98276addb0df2be82707
Lauren N. Roth <laurennicoleroth@gmail.com>
2016-05-20 14:04:39 -0400

    Re-add those missing files to make Xcode happy

--- ScavengerHunt/PlaceDetailSubview.swift
+++ ScavengerHunt/PlaceDetailSubview.swift
@@ -1,7 +1,16 @@
+//
+//  PlaceDetailSubview.swift
+//  ScavengerHunt
+//
+//  Created by Lauren Nicole Roth on 5/20/16.
+//  Copyright © 2016 Lauren Nicole Roth. All rights reserved.
+//
+
+import Foundation
 import UIKit
 
 class PlaceDetailSubview: UIView {
-
+    
     @IBOutlet var placeName: UILabel!
     @IBAction func buttonClicked(sender: AnyObject) {
     }
--- ScavengerHunt/StarRating.swift
+++ ScavengerHunt/StarRating.swift
@@ -1,4 +1,12 @@
+//
+//  StarRating.swift
+//  ScavengerHunt
+//
+//  Created by Lauren Nicole Roth on 5/20/16.
+//  Copyright © 2016 Lauren Nicole Roth. All rights reserved.
+//
 
+import Foundation
 import UIKit
 
 
@@ -10,30 +18,30 @@ class StarRating: UIImage {
         
         switch rating {
             
-            case 0.0:
-                ratingImage = UIImage(named:"zerostars")!
-            case 0..<1.0:
-                ratingImage = UIImage(named:"halfstars")!
-            case 1.0:
-                ratingImage = UIImage(named:"onestars")!
-            case 1.0..<2.0:
-                ratingImage = UIImage(named:"onehalfstars")!
-            case 2.0:
-                ratingImage = UIImage(named:"twostars")!
-            case 2.0..<3.0:
-                ratingImage = UIImage(named:"twohalfstars")!
-            case 3.0:
-                ratingImage = UIImage(named:"threestars")!
-            case 3.0..<4.0:
-                ratingImage = UIImage(named:"threehalfstars")!
-            case 4.0:
-                ratingImage = UIImage(named:"fourstars")!
-            case 4.0..<5.0:
-                ratingImage = UIImage(named:"fourhalfstars")!
-            case 5.0:
-                ratingImage = UIImage(named:"fivestars")!
-            default:
-                ratingImage = UIImage(named:"zerostars")!
+        case 0.0:
+            ratingImage = UIImage(named:"zerostars")!
+        case 0..<1.0:
+            ratingImage = UIImage(named:"halfstars")!
+        case 1.0:
+            ratingImage = UIImage(named:"onestars")!
+        case 1.0..<2.0:
+            ratingImage = UIImage(named:"onehalfstars")!
+        case 2.0:
+            ratingImage = UIImage(named:"twostars")!
+        case 2.0..<3.0:
+            ratingImage = UIImage(named:"twohalfstars")!
+        case 3.0:
+            ratingImage = UIImage(named:"threestars")!
+        case 3.0..<4.0:
+            ratingImage = UIImage(named:"threehalfstars")!
+        case 4.0:
+            ratingImage = UIImage(named:"fourstars")!
+        case 4.0..<5.0:
+            ratingImage = UIImage(named:"fourhalfstars")!
+        case 5.0:
+            ratingImage = UIImage(named:"fivestars")!
+        default:
+            ratingImage = UIImage(named:"zerostars")!
         }
         
         return ratingImage
@@ -41,6 +49,5 @@ class StarRating: UIImage {
     }
     
     
-
+    
 }
-
